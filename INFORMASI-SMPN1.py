d75529fed4f9899ce776954f9d9726692b9c4861
Mr-JAVXXZ <150129084+Mr-JAVXXZ@users.noreply.github.com>
2023-11-09 18:18:13 +0700

    Create INFORMASI-SMPN1.py

---
+++ INFORMASI-SMPN1.py
@@ -0,0 +1,34 @@
+import os
+import socket
+import time
+
+os.system("clear")
+os.system("figlet SMP 1 TEMON DIY")
+
+print "==============SMP NEGERI 1 TEMON ==============="
+print "Nama Sekolah    : SEKOLAH MENENGAH PERTAMA NEGERI 1 TEMON"
+print "Kode Pos Smp 1  : 55654"
+print "Kepala Sekolah: Sukaryana Mpd.Spd"
+print "Guru Pengampu   : 22"
+print "Siswa Laki-laki : 192"
+print "Siswa Perempuan : 194"
+print "Rombongan Belajar : 12"
+print "Kurikulum       : SMP 2013"
+print " Penyelenggaraan  : Sehari Penuh/5 hari"
+print "Manajemen Berbasis Sekolah : Tidak Ada"
+print "Semester Data  : 2023/2024-1"
+print "Akses Internet : Tidak Ada"
+print "Sumber Listrik : PLN"
+print "Daya Listrik   : 22"
+print "Luas Tanah     : 0 M²"
+print "Ruang Kelas    : 9"
+print "Laboratorium   : 2"
+print "Perpustakaan   : 1"
+print "Sanitasi Siswa : 10"
+print "==============================================="
+Exit =raw_input("KETIK YA UNTUK KELUAR = ")
+print "LOADING ....."
+time.sleep(2)
+print "DONE"
+time.sleep(1)
+os.system("clear")
